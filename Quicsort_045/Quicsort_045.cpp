754b081d7ae05cc28641af47d2c3ef5675024a37
adwitafernanda <adwitafernanda4@gmail.com>
2023-03-30 10:59:19 +0700

    membuat fungsi input

--- Quicsort_045/Quicsort_045.cpp
+++ Quicsort_045/Quicsort_045.cpp
@@ -7,3 +7,25 @@ int cmp_count = 0;
 int mov_count = 0;
 int n;
 
+void inpit() {
+	while (true)
+	{
+		cout << "Masukkan panjang element array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "Maksimum panjang array adalah 20" << endl;
+	}
+
+	cout << "\n-------------------" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n-------------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
